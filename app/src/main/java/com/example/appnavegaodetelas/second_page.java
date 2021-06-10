e6a5671eb3ec2928c32dafca44cc608b2b6685d3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-06-10 17:15:01 -0300

    Created: Function - navigate to first page

--- app/src/main/java/com/example/appnavegaodetelas/second_page.java
+++ app/src/main/java/com/example/appnavegaodetelas/second_page.java
@@ -2,7 +2,10 @@ package com.example.appnavegaodetelas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class second_page extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class second_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_page);
+        Button navigateor_button = (Button) findViewById(R.id.button);
+    }
+
+    public void navigate_to_first_page(View view){
+
+        Intent itent = new Intent(this, MainActivity.class);
+        startActivity(itent);
     }
 }

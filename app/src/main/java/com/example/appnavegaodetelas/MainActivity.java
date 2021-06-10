d9d54c785d849017f8f2227f7e1dfe1440a1e7bb
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-06-10 17:13:20 -0300

    Created: Function - navigate to second page

--- app/src/main/java/com/example/appnavegaodetelas/MainActivity.java
+++ app/src/main/java/com/example/appnavegaodetelas/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.appnavegaodetelas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button navigateor_button = (Button) findViewById(R.id.button);
     }
 
-    
+    public void navigate_to_second_page(View view){
+
+        Intent itent = new Intent(this, second_page.class);
+        startActivity(itent);
+    }
 }
